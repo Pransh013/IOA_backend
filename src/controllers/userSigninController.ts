80426e9ffa4f86d030d61d68fa4c0c33a60b1611
Pranshu Verma <pranshuverma1601@gmail.com>
2024-05-20 02:25:16 +0530

    signin route added

---
+++ src/controllers/userSigninController.ts
@@ -0,0 +1,48 @@
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import { dynamodb } from "../db";
+
+const jwtSecretKey = process.env.JWT_SECRET_KEY || "myjwtsecret";
+
+const userSigninController = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are required" });
+  }
+
+  try {
+    const params = {
+      TableName: "ioa_users",
+      IndexName: "email-index",
+      KeyConditionExpression: "email = :email",
+      ExpressionAttributeValues: {
+        ":email": email,
+      },
+    };
+
+    const result = await dynamodb.query(params).promise();
+
+    if (result.Items && result.Items.length > 0) {
+      const user = result.Items[0];
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).json({ error: "Invalid password" });
+      }
+
+      const token = jwt.sign({ userId: user.userId }, jwtSecretKey);
+
+      return res.status(200).json({ message: "Login successful", token });
+    } else {
+      return res.status(401).json({ error: "Invalid email" });
+    }
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export default userSigninController;
